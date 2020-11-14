3e9f33af2d84989371c90e1ef02bad38bdab4c0f
Trần Xuân Việt <viet.tx@3si.vn>
2020-11-14 14:09:56 +0700

    imp

--- src/main/java/com/nakun/polls/model/Choice.java
+++ src/main/java/com/nakun/polls/model/Choice.java
@@ -1,2 +1,69 @@
-package com.nakun.polls.model;public class Choice {
+package com.nakun.polls.model;
+
+import com.nakun.polls.model.audit.UserDateAudit;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "choices")
+public class Choice extends UserDateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    public Choice() {
+    }
+
+    public Choice(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Poll getPoll() {
+        return poll;
+    }
+
+    public void setPoll(Poll poll) {
+        this.poll = poll;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Choice choice = (Choice) o;
+        return Objects.equals(id, choice.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/nakun/polls/model/dto/ChoiceVoteCount.java
+++ src/main/java/com/nakun/polls/model/dto/ChoiceVoteCount.java
@@ -1,2 +1,27 @@
-package com.nakun.polls.model.dto;public class ChoiceVoteCount {
+package com.nakun.polls.model.dto;
+
+public class ChoiceVoteCount {
+    private Long choiceId;
+    private Long voteCount;
+
+    public ChoiceVoteCount(Long choiceId, Long voteCount) {
+        this.choiceId = choiceId;
+        this.voteCount = voteCount;
+    }
+
+    public Long getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(Long choiceId) {
+        this.choiceId = choiceId;
+    }
+
+    public Long getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Long voteCount) {
+        this.voteCount = voteCount;
+    }
 }
